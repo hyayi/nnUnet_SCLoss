ea75b23c7c29db5608f3e01cbb99c2bb17299bda
hyayi <98936899+hyayi@users.noreply.github.com>
2025-04-23 17:03:20 +0900

    Update nnUNetTrainerSCLoss.py

--- nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainerSCLoss.py
+++ nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainerSCLoss.py
@@ -153,4 +153,67 @@ class nnUNetTrainerDCSCLossTip(nnUNetTrainer):
             weights = weights / weights.sum()
             loss = DeepSupervisionWrapper(loss, weights)
 
+
+        return loss
+class nnUNetTrainerDCCESCLossBG(nnUNetTrainer):
+    def _build_loss(self):
+        loss = DC_and_CE_SCloss({'batch_dice': self.configuration_manager.batch_dice,
+                                'smooth': 1e-5, 'do_bg': False, 'ddp': self.is_ddp}, {},{'k': 2,'alpha': 1.0,'target_classes': [0]},weight_ce=1, weight_dice=1,
+                                ignore_label=self.label_manager.ignore_label, dice_class=MemoryEfficientSoftDiceLoss)
+
+        if self._do_i_compile():
+            loss.dc = torch.compile(loss.dc)
+
+        if self.enable_deep_supervision:
+            deep_supervision_scales = self._get_deep_supervision_scales()
+            weights = np.array([1 / (2 ** i) for i in range(len(deep_supervision_scales))])
+            if self.is_ddp and not self._do_i_compile():
+                weights[-1] = 1e-6
+            else:
+                weights[-1] = 0
+            weights = weights / weights.sum()
+            loss = DeepSupervisionWrapper(loss, weights)
+
+        return loss
+class nnUNetTrainerDCCESCLossTube(nnUNetTrainer):
+    def _build_loss(self):
+        loss = DC_and_CE_SCloss({'batch_dice': self.configuration_manager.batch_dice,
+                                'smooth': 1e-5, 'do_bg': False, 'ddp': self.is_ddp}, {},{'k': 2,'alpha': 1.0,'target_classes': [1]},weight_ce=1, weight_dice=1,
+                                ignore_label=self.label_manager.ignore_label, dice_class=MemoryEfficientSoftDiceLoss)
+
+        if self._do_i_compile():
+            loss.dc = torch.compile(loss.dc)
+
+        if self.enable_deep_supervision:
+            deep_supervision_scales = self._get_deep_supervision_scales()
+            weights = np.array([1 / (2 ** i) for i in range(len(deep_supervision_scales))])
+            if self.is_ddp and not self._do_i_compile():
+                weights[-1] = 1e-6
+            else:
+                weights[-1] = 0
+            weights = weights / weights.sum()
+            loss = DeepSupervisionWrapper(loss, weights)
+
+        return loss
+
+
+class nnUNetTrainerDCCESCLossTip(nnUNetTrainer):
+    def _build_loss(self):
+        loss = DC_and_CE_SCloss({'batch_dice': self.configuration_manager.batch_dice,
+                                'smooth': 1e-5, 'do_bg': False, 'ddp': self.is_ddp}, {},{'k': 2,'alpha': 1.0,'target_classes': [2]},weight_ce=1, weight_dice=1,
+                                ignore_label=self.label_manager.ignore_label, dice_class=MemoryEfficientSoftDiceLoss)
+
+        if self._do_i_compile():
+            loss.dc = torch.compile(loss.dc)
+
+        if self.enable_deep_supervision:
+            deep_supervision_scales = self._get_deep_supervision_scales()
+            weights = np.array([1 / (2 ** i) for i in range(len(deep_supervision_scales))])
+            if self.is_ddp and not self._do_i_compile():
+                weights[-1] = 1e-6
+            else:
+                weights[-1] = 0
+            weights = weights / weights.sum()
+            loss = DeepSupervisionWrapper(loss, weights)
+
         return loss
