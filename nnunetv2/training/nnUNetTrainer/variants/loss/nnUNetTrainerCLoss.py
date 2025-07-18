5e868e15bf74bb34645b2344b3fcb8b1721b73c6
hyayi <inseo5568@gmail.com>
2025-07-18 04:44:40 +0000

    add 2000 epoch trainer

--- nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainerCLoss.py
+++ nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainerCLoss.py
@@ -2,11 +2,29 @@ import numpy as np
 import torch
 from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
 from nnunetv2.training.loss.deep_supervision import DeepSupervisionWrapper
-
+from nnunetv2.training.nnUNetTrainer.variants.data_augmentation.nnUNetTrainerCole import nnUNetTrainerCole 
 from nnunetv2.training.loss.closs import CLoss
 
 
 class nnUNetTrainerCLoss_gamma0p2(nnUNetTrainer):
+    def _build_loss(self):
+        loss = CLoss(gamma=0.2)
+
+        if self.enable_deep_supervision:
+            deep_supervision_scales = self._get_deep_supervision_scales()
+
+            # we give each output a weight which decreases exponentially (division by 2) as the resolution decreases
+            # this gives higher resolution outputs more weight in the loss
+            weights = np.array([1 / (2 ** i) for i in range(len(deep_supervision_scales))])
+            weights[-1] = 0
+
+            # we don't use the lowest 2 outputs. Normalize weights so that they sum to 1
+            weights = weights / weights.sum()
+            # now wrap the loss
+            loss = DeepSupervisionWrapper(loss, weights)
+        return loss
+
+class nnUNetTrainerCLoss_gamma0p2Cole(nnUNetTrainerCole):
     def _build_loss(self):
         loss = CLoss(gamma=0.2)
 
