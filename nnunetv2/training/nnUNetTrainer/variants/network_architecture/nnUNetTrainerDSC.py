54adf512637a12b6877b48440818de0a734c7d88
hyayi <98936899+hyayi@users.noreply.github.com>
2025-10-21 14:29:45 +0900

    Update nnUNetTrainerDSC.py

--- nnunetv2/training/nnUNetTrainer/variants/network_architecture/nnUNetTrainerDSC.py
+++ nnunetv2/training/nnUNetTrainer/variants/network_architecture/nnUNetTrainerDSC.py
@@ -16,17 +16,6 @@ class nnUNetTrainerDSCResidual(nnUNetTrainer):
     - 파라미터 처리를 간소화하고 nnU-Net의 plans.json 설정을 존중합니다.
     - Deep Supervision 설정을 동적으로 받아옵니다.
     """
-    def __init__(
-        self,
-        plans: dict,
-        configuration: str,
-        fold: int,
-        dataset_json: dict,
-        device: torch.device = torch.device("cuda"),
-    ):
-        super().__init__(plans, configuration, fold, dataset_json, device)
-        self.enable_deep_supervision = False
-    @staticmethod
     def build_network_architecture(architecture_class_name: str,
                                    arch_init_kwargs: dict,
                                    arch_init_kwargs_req_import: Union[List[str], Tuple[str, ...]],
